5ddb16c9d2877532795ecd9a8aaa17a369a7cd10
Luis Trevizon <ljtrevizon@gmail.com>
2024-08-07 18:44:04 -0400

    agregar RESULTADOS_2024_CSV_V2, modificando el sql y la base de datos

--- busqueda de datos/archivo.sql
+++ busqueda de datos/archivo.sql
@@ -1,1 +1,4 @@
-select EDO,MUN,PAR,CENTRO, count(MESA), sum(EG) as EG, URL from resultados  GROUP by CENTRO order by EG desc
+select EDO,MUN,PAR,CENTRO, count(MESA) as "MESA/S", sum(EG) as "VOTOS A EDMUNDO", 
+sum(NM) as "VOTOS A MADURO",sum(LM) as "VOTOS A LUIS MARTINEZ",sum(AE) as "VOTOS A ECARRI" from resultados    
+group by CENTRO
+order by sum(AE) desc
--- busqueda de datos/prueba.py
+++ busqueda de datos/prueba.py
@@ -2,10 +2,11 @@ import pandas as pd
 import sqlite3
 from pathlib import Path
 
-
-Path('my_data.db').touch()
-conn = sqlite3.connect('my_data.db')
+conn = sqlite3.connect('busqueda de datos\my_data.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE resultados (id int,COD_EDO int,EDO text, MUN text, PAR text, CENTRO text, MESA int, EG int, URL text)''')
-base = pd.read_csv("csv/RESULTADOS_CENTRO_MESA_EG.csv")
-base.to_sql('resultados', conn, if_exists='append', index=False)
+
+c.execute('''update sqlite_sequence set seq=""''')
+c.execute('''DELETE from resultados''')
+original = pd.read_csv("csv\RESULTADOS_2024_CSV_V2.csv", encoding='latin-1')
+original.to_sql('resultados', conn, if_exists='append', index=False)
+print("Listo")
