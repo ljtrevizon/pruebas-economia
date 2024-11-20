4700ce43ab8a58d86a3c388a7be62816efc20be9
Luis Trevizon <ljtrevizon@gmail.com>
2024-11-20 14:49:44 -0400

    acomodando algo

--- interseccion.py
+++ interseccion.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 titulo = input("Introduzca el titulo: ")
 x = Symbol("x")
-y=Symbol("y")
+y = Symbol("y")
 titulo_fig = input("Introduzca el titulo del archivo: ")
 f_d = sympify(input("Ingrese una funcion de demanda x: "))
 f_d_eq = Eq(f_d, y)
@@ -11,8 +11,7 @@ f_d_zero = Eq(f_d, 0)
 f_o = sympify(input("Ingrese una funcion de oferta x: "))
 f_o_eq = Eq(f_o, y)
 corte_x = round(nsolve(f_d_zero, 1))
-print(corte_x)
-solution = nsolve([f_o_eq, f_d_eq], [x, y],[5,5],dict=True)
+solution = nsolve([f_o_eq, f_d_eq], [x, y], [5, 5], dict=True)
 point_x = []
 point_y_d = []
 point_y_o = []
@@ -23,11 +22,11 @@ for i in range(corte_x+5):
     point_y_d.append(f_d.subs(x, i))
     point_y_o.append(f_o.subs(x, i))
 plt.scatter(point_neutral_x, point_neutral_y, color="black")
-plt.plot(point_x, point_y_o,color="green")
-plt.plot(point_x, point_y_d,color="red")
+plt.plot(point_x, point_y_o, color="green")
+plt.plot(point_x, point_y_d, color="red")
 plt.title(titulo)
 plt.ylabel("Precio")
 plt.xlabel("Unidades")
-plt.legend(["Punto de Equilibrio","Oferta","Demanda"])
+plt.legend(["Punto de Equilibrio", "Oferta", "Demanda"])
 plt.savefig("graficos/"+titulo_fig+".png", bbox_inches="tight")
 plt.show()
